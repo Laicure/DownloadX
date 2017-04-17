d4cf9cb6f44f0586052c0e2ee65ee9eb763d5287
Laicure <Laicure@outlook.com>
2017-04-17 23:16:44 +0800

    initial sync

---
+++ DownloadX/Forms/MainX.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainX
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txUrl = New System.Windows.Forms.TextBox()
+        Me.pgBar = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txUrl
+        '
+        Me.txUrl.BackColor = System.Drawing.Color.White
+        Me.txUrl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txUrl.ForeColor = System.Drawing.Color.Black
+        Me.txUrl.Location = New System.Drawing.Point(4, 4)
+        Me.txUrl.Name = "txUrl"
+        Me.txUrl.Size = New System.Drawing.Size(343, 22)
+        Me.txUrl.TabIndex = 1
+        '
+        'pgBar
+        '
+        Me.pgBar.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pgBar.Location = New System.Drawing.Point(0, 0)
+        Me.pgBar.Name = "pgBar"
+        Me.pgBar.Size = New System.Drawing.Size(434, 30)
+        Me.pgBar.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(353, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(77, 22)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Download!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MainX
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(434, 155)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txUrl)
+        Me.Controls.Add(Me.pgBar)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "MainX"
+        Me.Padding = New System.Windows.Forms.Padding(1)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MainX"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txUrl As System.Windows.Forms.TextBox
+    Friend WithEvents pgBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
